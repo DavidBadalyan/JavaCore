c715571f128912714c5eb4778ca030fa7644e787
David Badalyan <david1codzilla@gmail.com>
2023-08-30 19:24:20 +0400

    homework(figure painter)

--- src/classwork/chapter3/AutoArray.java
+++ src/classwork/chapter3/AutoArray.java
@@ -1,5 +1,7 @@
 package classwork.chapter3;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class AutoArray {
     public static void main(String[] args) {
         int[] month_days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
