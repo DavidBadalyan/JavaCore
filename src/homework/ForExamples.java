98e231adb20f47a21d937c29e04473d9dc9d6620
David Badalyan <david1codzilla@gmail.com>
2023-09-03 10:37:42 +0400

    homework(ForExamples) + examples from the book

--- src/homework/ForExamples.java
+++ src/homework/ForExamples.java
@@ -14,7 +14,7 @@ public class ForExamples {
 
         //task 2: print all the even numbers 1 - 100 using the % operator
         for (int i = 0; i < 100; i++) {
-            if((i + 1) % 2 == 0) {
+            if ((i + 1) % 2 == 0) {
                 System.out.print((i + 1) + " ");
             }
         }
@@ -22,13 +22,13 @@ public class ForExamples {
         System.out.println("\n\n");
 
         //task 3: print the biggest number in an array of integers
-        int[] array = {2,5,8,4,9,3,7};
+        int[] array = {2, 5, 8, 4, 9, 3, 7};
         int max = array[0];
 
         for (int i = 1; i < 7; i++) {
-                if(array[i] > max) {
-                    max = array[i];
-                }
+            if (array[i] > max) {
+                max = array[i];
+            }
         }
 
         System.out.println("The biggest number in {2,5,8,4,9,3,7} array is: " + max);
